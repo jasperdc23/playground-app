b7d9ab28003307953a2979dc3fde8b99b772308e
Jasper Caparas <jaspercaparas@Jaspers-MacBook-Pro.local>
2026-03-30 19:50:36 +0800

    Pass DATABASE_URL to PrismaClient constructor for v7

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient();
+  globalForPrisma.prisma ?? new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
